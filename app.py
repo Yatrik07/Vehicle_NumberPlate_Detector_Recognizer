616755df33dae523fa7fd908091852b3679f8d27
Yatrik07 <86146170+Yatrik07@users.noreply.github.com>
2022-12-06 18:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
-from werkzeug.utils import secure_filename
 import tensorflow as tf
 import cv2
 import numpy as np
@@ -10,6 +9,9 @@ from werkzeug.utils import secure_filename
 import os
 from prediction.make_predictions import *
 from image_processing.preprocessing import showImageWithAnnot, resize
+from OCR.ocr import read_number, apply_easyocr
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 app =Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/files'
@@ -49,9 +51,11 @@ def prediction_page():
             # return send_file(path , mimetype='image/gif')
             # print(path)
             print(detected_path, cropped_path)
-            return render_template("img.html", input = r'static\files\resized_input_img.jpg', detected = detected_path, cropped = cropped_path) # , img_path_final = final_path
 
+            text = read_number(cropped)
+            easyocr_text = apply_easyocr((cropped))
 
+            return render_template("img.html", input = r'static\files\resized_input_img.jpg', detected = detected_path, cropped = cropped_path, text=text, easyocr_text= easyocr_text) # , img_path_final = final_path
 
     # except Exception as e:
     #     return 'There is some error : \n' + str(e)
